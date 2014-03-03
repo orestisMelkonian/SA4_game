5e0897ea121c91a38457c532e24fcf67ccbb7f22
courado <stevengatsios@gmail.com>
2014-03-03 17:00:48 +0100

    Update on hexagon

--- src/main/java/ptoma/hexoral/Hexagon.java
+++ src/main/java/ptoma/hexoral/Hexagon.java
@@ -3,10 +3,14 @@ package ptoma.hexoral;
 public class Hexagon {
 	static public enum directions {TOP_LEFT,TOP_RIGHT,RIGHT,BOTTOM_RIGHT,BOTTOM_LEFT,LEFT};
 	static public enum type {SEA,LAND};
+	/**
+	 * Type of terrain
+	 */
 	private type terrainType;
 	
-	public Hexagon(type _terrainType) {
-		terrainType = _terrainType;
+	
+	public Hexagon(type terrainType) {
+		this.terrainType = terrainType;
 	}
 	
 	@Override
@@ -15,10 +19,21 @@ public class Hexagon {
 	}
 	
 	/**
+	 * @author Stefanos
 	 * Return the type of the Hexagon like SEA or LAND etc...
 	 * @return String The type of the Hexagon
 	 */
 	public String getType() {
 		return terrainType.toString();
+
+	}
+	
+	/**
+	 * Setter for type of hexagon
+	 * @author Stefanos
+	 * @param terrainType of type Hexagon.type
+	 */
+	public void setType(type terrainType) {
+		this.terrainType = terrainType;
 	}
 }
