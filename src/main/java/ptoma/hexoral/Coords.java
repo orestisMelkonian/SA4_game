784831da3d8e305c02dec654652e2ff0d9c194d8
courado <stevengatsios@gmail.com>
2014-03-03 17:09:02 +0100

    Added coords class to represent coordinates easily

---
+++ src/main/java/ptoma/hexoral/Coords.java
@@ -0,0 +1,34 @@
+package ptoma.hexoral;
+
+public class Coords {
+	/**
+	 *  X coordinate
+	 */
+	public int x;
+	/**
+	 * Y coordinate
+	 */
+	public int y;
+	
+	/**
+	 * @param x The x coordinate
+	 * @param y The y coordinate
+	 */
+	public Coords(int x,int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * Default constructor x=y=0
+	 */
+	public Coords() {
+		this.x = this.y = 0;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Coords c = (Coords) obj;
+		return this.x == c.x && this.y ==c.y; 
+	}
+}
