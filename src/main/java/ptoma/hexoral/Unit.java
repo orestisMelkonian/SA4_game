a5a7281add9b24d64344452b9b311d583860713f
Lokesh Menghani <menghl@usi.ch>
2014-03-04 15:49:06 +0100

    methods for Unit interface

--- src/main/java/ptoma/hexoral/Unit.java
+++ src/main/java/ptoma/hexoral/Unit.java
@@ -2,5 +2,10 @@ package ptoma.hexoral;
 
 public interface Unit {
 	
+	public Coords move(Coords moveCoord);
+	public void attack(Coords attackCoord);
+	public Coords follow(Coords followCoord);
+	
+	
 
 }
