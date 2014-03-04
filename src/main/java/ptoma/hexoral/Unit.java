20a679d62a086c3988c8c2cf8b63ea0a4dccaf87
Vitor Carmo Vannuchi <carmov@usi.ch>
2014-03-04 16:36:05 +0100

    inicialization problems

--- src/main/java/ptoma/hexoral/Unit.java
+++ src/main/java/ptoma/hexoral/Unit.java
@@ -1,8 +1,8 @@
 package ptoma.hexoral;
 
 public interface Unit {
-	boolean isDestroyed;
-	Soldiers soldierCount;
+	//boolean isDestroyed;
+	//Soldiers soldierCount;
 	public Coords move(Coords moveCoord);
 	public void attack(Coords attackCoord);
 	public Coords follow(Coords followCoord);
