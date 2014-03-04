a220cd920ede37d84c3ae4927f9d87e39159a3e8
Vitor Carmo Vannuchi <carmov@usi.ch>
2014-03-04 16:25:44 +0100

    Unit test

--- src/main/java/ptoma/hexoral/Unit.java
+++ src/main/java/ptoma/hexoral/Unit.java
@@ -2,7 +2,7 @@ package ptoma.hexoral;
 
 public interface Unit {
 	
-	//Soldiers soldierCount;
+	Soldiers soldierCount;
 	public Coords move(Coords moveCoord);
 	public void attack(Coords attackCoord);
 	public Coords follow(Coords followCoord);
