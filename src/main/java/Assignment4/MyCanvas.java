5622f828b8425e689b1ef52ad552840fee52d098
hanieh soleimani <hanieh.s@hotmail.com>
2014-03-10 15:49:42 +0100

    MyCanvas object is created *Hanieh*

--- src/main/java/Assignment4/MyCanvas.java
+++ src/main/java/Assignment4/MyCanvas.java
@@ -1,5 +1,34 @@
+
+/*author :Hanieh */
+
 package Assignment4;
 
-public class MyCanvas {
+import java.awt.Canvas;
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class MyCanvas extends Canvas  {
+	
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	MyCanvas() {
+		
+		
+		
+		JFrame AppWin = new JFrame("Map Generator");
+		AppWin.setBackground(Color.WHITE );
+		AppWin.setSize(800, 600);
+		AppWin.setVisible(true);
+		AppWin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		AppWin.add(this);
+		setVisible(true);
+		
+	}
+	
 
 }
