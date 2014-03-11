13cdd760c33b57ef85bcc657f41f8da14ea9fa7c
hanieh soleimani <hanieh.s@hotmail.com>
2014-03-11 13:35:28 +0100

    hey guys, Mymap class *hanieh*

---
+++ src/main/java/Assignment4/Mymap.java
@@ -0,0 +1,54 @@
+package Assignment4;
+
+public class Mymap {
+	
+	private float width;
+	private float height;
+	
+	
+   public Mymap(float height, float width) {
+	// TODO Auto-generated constructor stub
+	   
+	   this.width = width;
+	   this.height = height;
+   }
+   
+  
+   
+   	public float getHeight() {
+	return height;
+   }
+   
+   
+    public float getWidth() {
+		return width;
+	}
+    
+    
+    public void setHeight(float height) {
+		this.height = height;
+	}
+    
+    
+    public void setWidth(float width) {
+		this.width = width;
+	}
+    
+    /** 
+     * author: hanieh 
+	 * Create noise in a 2D space.
+	 * return :A noisy value at the given position
+	 */
+   
+    public float noise(float x,float y){
+	   
+	   return 0;
+	   
+   }
+
+   
+   
+   
+
+
+}
