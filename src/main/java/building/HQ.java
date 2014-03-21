a813a514019d82f2b2c118e01d17a99220bffcfe
Lokesh Menghani <menghl@usi.ch>
2014-03-21 16:36:21 +0100

    HQ

--- src/main/java/building/HQ.java
+++ src/main/java/building/HQ.java
@@ -1,7 +1,9 @@
 package building;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Container;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,46 +13,84 @@ import javax.swing.JPanel;
 
 import ptoma.hexoral.units.Unit;
 import ptoma.hexoral.user.Player;
+import ptoma.hexoral.user.User;
 
 public class HQ {
 	
-	private Player p;
-	
-	public static void main(String[] args){
-		new HQ();
-	}
-		
-	
-	public HQ(){
-		createGUI();
-	}
-	
-	public Unit createUnit(){
-		return new Unit(p);
-	}
+	/**
+	 * Refernce to the owner of the HQ
+	 */
+	private Player owner;
 
-	public void createGUI(){
-		//System.out.println("GAY");
-		final JFrame frame = new JFrame("HQ");
-		JPanel leftPanel = new JPanel(new BorderLayout());
-		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		JButton HQButton = new JButton("HQ");
-		leftPanel.add(HQButton, BorderLayout.NORTH);
-		frame.add(leftPanel, BorderLayout.WEST);
+	/**
+	 * The position of the HQ
+	 */
+	private Point position;
+	
+	/**
+	 * The health of the HQ.
+	 */
+	protected long health;
 		
-		HQButton.addActionListener(new ActionListener() { 
-            public void actionPerformed(ActionEvent ae) {
-            	JButton createUnitButton = new JButton("Create Unit");
-            	JPanel rightPanel = new JPanel(new BorderLayout());
-            	rightPanel.add(createUnitButton, BorderLayout.NORTH);
-            	Container content = frame.getContentPane();
-            	content.add(rightPanel);
-            	content.repaint();
-            }     
-        });
-		frame.setSize(500,500);
-		
-		frame.setVisible(true);
-		
+	
+	public HQ(Player owner, int x, int y){
+		this.owner = owner;
+		this.position = new Point(x, y);
 	}
+	
+	/**
+	 * Returns a reference of the owner of the unit.
+	 * 
+	 * @return Player the owner.
+	 */
+	public Player getOwner() {
+		return owner;
+	}
+	
+	public Point getPosition() {
+		return position;
+	}
+	
+
+	public long getHealth(){
+		return health;
+	}
+	
+//	public Unit createNewUnit(){
+//		return new Unit(owner, position);
+//	}
+//	
+
+//	public void createGUI(){
+//		//System.out.println("GAY");
+//		final JFrame frame = new JFrame("HQ");
+//		final JPanel leftPanel = new JPanel(new BorderLayout());
+//		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+//		JButton HQButton = new JButton("HQ");
+//		leftPanel.add(HQButton, BorderLayout.NORTH);
+//		final JPanel rightPanel = new JPanel(new BorderLayout());
+//		HQButton.addActionListener(new ActionListener() { 
+//            public void actionPerformed(ActionEvent ae) {
+//            	JButton createUnitButton = new JButton("Create Unit");
+//            	rightPanel.add(createUnitButton, BorderLayout.NORTH);
+//            	Container content = frame.getContentPane();
+//            	content.add(rightPanel);
+//            	content.repaint();
+//            	createUnitButton.addActionListener(new ActionListener() { 
+//                    public void actionPerformed(ActionEvent ae) {
+//                    	Unit unit = createUnit();
+//                    	
+//                    }     
+//                });
+//            }     
+//        });
+//		Container content = frame.getContentPane();
+//    	content.add(rightPanel);
+//    	
+//    	frame.add(leftPanel, BorderLayout.WEST);
+//		frame.setSize(500,500);
+//		
+//		frame.setVisible(true);
+//		
+//	}
 }
