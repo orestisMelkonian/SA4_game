6a874faaf6683e33cf7587e8ae742529ff4c3b3c
Vitor Carmo Vannuchi <carmov@usi.ch>
2014-03-25 15:37:57 +0100

    \o/

--- src/main/java/ptoma/hexoral/GameUI.java
+++ src/main/java/ptoma/hexoral/GameUI.java
@@ -94,47 +94,10 @@ public class GameUI {
 		
 		JPanel userInfoTab = new JPanel();
 		tabbedPane.addTab("User Info", null, userInfoTab, null);
-		GridBagLayout gbl_userInfoTab = new GridBagLayout();
-		gbl_userInfoTab.columnWidths = new int[] {100, 0, 0};
-		gbl_userInfoTab.rowHeights = new int[] {15, 15, 0};
-		gbl_userInfoTab.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gbl_userInfoTab.rowWeights = new double[]{0.0, 0.0};
-		userInfoTab.setLayout(gbl_userInfoTab);
+		userInfoTab.setLayout(new GridLayout(0, 1, 0, 0));
 		
-		JLabel unitCounterFxd = new JLabel("Unit counter");
-		unitCounterFxd.setHorizontalAlignment(SwingConstants.LEFT);
-		GridBagConstraints gbc_unitCounterFxd = new GridBagConstraints();
-		gbc_unitCounterFxd.fill = GridBagConstraints.BOTH;
-		gbc_unitCounterFxd.insets = new Insets(0, 0, 5, 5);
-		gbc_unitCounterFxd.gridx = 0;
-		gbc_unitCounterFxd.gridy = 0;
-		userInfoTab.add(unitCounterFxd, gbc_unitCounterFxd);
-		
-		JLabel unitCounterValue = new JLabel("0");
-		unitCounterValue.setHorizontalAlignment(SwingConstants.LEFT);
-		GridBagConstraints gbc_unitCounterValue = new GridBagConstraints();
-		gbc_unitCounterValue.fill = GridBagConstraints.BOTH;
-		gbc_unitCounterValue.insets = new Insets(0, 0, 5, 0);
-		gbc_unitCounterValue.gridx = 1;
-		gbc_unitCounterValue.gridy = 0;
-		userInfoTab.add(unitCounterValue, gbc_unitCounterValue);
-		
-		JLabel buildCounterFxd = new JLabel("Build counter");
-		buildCounterFxd.setHorizontalAlignment(SwingConstants.LEFT);
-		GridBagConstraints gbc_buildCounterFxd = new GridBagConstraints();
-		gbc_buildCounterFxd.fill = GridBagConstraints.BOTH;
-		gbc_buildCounterFxd.insets = new Insets(0, 0, 0, 5);
-		gbc_buildCounterFxd.gridx = 0;
-		gbc_buildCounterFxd.gridy = 1;
-		userInfoTab.add(buildCounterFxd, gbc_buildCounterFxd);
-		
-		JLabel buildCounterValue = new JLabel("0");
-		buildCounterValue.setHorizontalAlignment(SwingConstants.LEFT);
-		GridBagConstraints gbc_buildCounterValue = new GridBagConstraints();
-		gbc_buildCounterValue.fill = GridBagConstraints.BOTH;
-		gbc_buildCounterValue.gridx = 1;
-		gbc_buildCounterValue.gridy = 1;
-		userInfoTab.add(buildCounterValue, gbc_buildCounterValue);
+		JList armySummaryList = new JList();
+		userInfoTab.add(armySummaryList);
 		
 		JPanel scheduleTab = new JPanel();
 		tabbedPane.addTab("Schedule Actions", null, scheduleTab, null);
@@ -142,8 +105,8 @@ public class GameUI {
 		JList scheduleList = new JList();
 		scheduleTab.add(scheduleList);
 		
-		JPanel unitTab = new JPanel();
-		tabbedPane.addTab("Unit Info", null, unitTab, null);
+		JPanel unitInfoTab = new JPanel();
+		tabbedPane.addTab("Unit Info", null, unitInfoTab, null);
 	}
 
 }
